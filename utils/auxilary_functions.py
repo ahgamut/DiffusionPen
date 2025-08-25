abb62c477e23c29d14b1796cbbb3ad5b40aeaeac
Gautham <ahgamut@gmail.com>
2025-08-25 09:11:20 -0500

    formatting

--- utils/auxilary_functions.py
+++ utils/auxilary_functions.py
@@ -4,32 +4,36 @@ from skimage.transform import resize
 import cv2
 
 
-def affine_transformation(img, m=1.0, s=.2, border_value=None):
+def affine_transformation(img, m=1.0, s=0.2, border_value=None):
     h, w = img.shape[0], img.shape[1]
-    src_point = np.float32([[w / 2.0, h / 3.0],
-                            [2 * w / 3.0, 2 * h / 3.0],
-                            [w / 3.0, 2 * h / 3.0]])
-    random_shift = m + np.random.uniform(-1.0, 1.0, size=(3,2)) * s
+    src_point = np.float32(
+        [[w / 2.0, h / 3.0], [2 * w / 3.0, 2 * h / 3.0], [w / 3.0, 2 * h / 3.0]]
+    )
+    random_shift = m + np.random.uniform(-1.0, 1.0, size=(3, 2)) * s
     dst_point = src_point * random_shift.astype(np.float32)
     transform = cv2.getAffineTransform(src_point, dst_point)
     if border_value is None:
         border_value = np.median(img)
-    warped_img = cv2.warpAffine(img, transform, dsize=(w, h), borderValue=float(border_value))
+    warped_img = cv2.warpAffine(
+        img, transform, dsize=(w, h), borderValue=float(border_value)
+    )
     return warped_img
+
 
 def image_resize(img, height=None, width=None):
 
     if height is not None and width is None:
         scale = float(height) / float(img.shape[0])
-        width = int(scale*img.shape[1])
+        width = int(scale * img.shape[1])
 
     if width is not None and height is None:
         scale = float(width) / float(img.shape[1])
-        height = int(scale*img.shape[0])
+        height = int(scale * img.shape[0])
 
     img = resize(image=img, output_shape=(height, width)).astype(np.float32)
 
     return img
+
 
 def image_resize_PIL(img, height=None, width=None):
     if height is None and width is None:
@@ -51,23 +55,23 @@ def image_resize_PIL(img, height=None, width=None):
 
     # Resize the image
     resized_img = img.resize((new_width, new_height))
-    #resized_img.save('res.png')
+    # resized_img.save('res.png')
     return resized_img
 
 
-def centered(word_img, tsize, centering=(.5, .5), border_value=None):
+def centered(word_img, tsize, centering=(0.5, 0.5), border_value=None):
 
     height = tsize[0]
     width = tsize[1]
 
     xs, ys, xe, ye = 0, 0, width, height
-    diff_h = height-word_img.shape[0]
+    diff_h = height - word_img.shape[0]
     if diff_h >= 0:
         pv = int(centering[0] * diff_h)
-        padh = (pv, diff_h-pv)
+        padh = (pv, diff_h - pv)
     else:
         diff_h = abs(diff_h)
-        ys, ye = diff_h/2, word_img.shape[0] - (diff_h - diff_h/2)
+        ys, ye = diff_h / 2, word_img.shape[0] - (diff_h - diff_h / 2)
         padh = (0, 0)
     diff_w = width - word_img.shape[1]
     if diff_w >= 0:
@@ -81,25 +85,26 @@ def centered(word_img, tsize, centering=(.5, .5), border_value=None):
     if border_value is None:
         border_value = np.median(word_img)
 
-    word_img = np.pad(word_img[ys:ye, xs:xe], ((0, 0), padw), 'constant', constant_values=border_value)
-    
+    word_img = np.pad(
+        word_img[ys:ye, xs:xe], ((0, 0), padw), "constant", constant_values=border_value
+    )
+
     return word_img
 
 
+def centered_PIL(word_img, tsize, centering=(0.5, 0.5), border_value=None):
 
-def centered_PIL(word_img, tsize, centering=(.5, .5), border_value=None):
-    
     height = tsize[0]
     width = tsize[1]
-    #print('word_img.size', word_img.size)
+    # print('word_img.size', word_img.size)
     xs, ys, xe, ye = 0, 0, width, height
-    diff_h = height-word_img.height
+    diff_h = height - word_img.height
     if diff_h >= 0:
         pv = int(centering[0] * diff_h)
-        padh = (pv, diff_h-pv)
+        padh = (pv, diff_h - pv)
     else:
         diff_h = abs(diff_h)
-        ys, ye = diff_h/2, word_img.height - (diff_h - diff_h/2)
+        ys, ye = diff_h / 2, word_img.height - (diff_h - diff_h / 2)
         padh = (0, 0)
     diff_w = width - word_img.width
     if diff_w >= 0:
@@ -112,11 +117,9 @@ def centered_PIL(word_img, tsize, centering=(.5, .5), border_value=None):
 
     if border_value is None:
         border_value = np.median(word_img)
-    
 
-    res = Image.new('RGB', (width, height), color = (255, 255, 255))
-  
+    res = Image.new("RGB", (width, height), color=(255, 255, 255))
+
     res.paste(word_img, (padw[0], padh[0]))
-    
-    
+
     return res
