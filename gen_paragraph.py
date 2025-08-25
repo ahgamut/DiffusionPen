6231b7f63fa80968838589b17b42c78b911478f9
Gautham <ahgamut@gmail.com>
2025-08-25 10:45:19 -0500

    separate script for paragrah generation

--- gen_paragraph.py
+++ gen_paragraph.py
@@ -23,6 +23,11 @@ from torchvision.utils import save_image
 from torch.nn import DataParallel
 from transformers import CanineModel, CanineTokenizer
 
+torch.cuda.empty_cache()
+OUTPUT_MAX_LEN = 95  # + 2  # <GO>+groundtruth+<END>
+IMG_WIDTH = 256
+IMG_HEIGHT = 64
+
 
 def setup_logging(args):
     # os.makedirs("models", exist_ok=True)
@@ -61,9 +66,15 @@ def crop_whitespace_width(img):
     return np.array(rect)
 
 
+def file_check(fname):
+    if os.path.isfile(fname):
+        return fname
+    raise RuntimeError(f"{fname} is not a file")
+
+
 def main():
     """Main function"""
-    parser = argparse.ArgumentParser("diffusionpen-singleword")
+    parser = argparse.ArgumentParser("diffusion-paragraph")
     parser.add_argument(
         "--model_name",
         type=str,
@@ -94,7 +105,7 @@ def main():
     parser.add_argument(
         "--stable_dif_path", type=str, default="./stable-diffusion-v1-5"
     )
-    parser.add_argument("--sampling_word", type=str, default="hello")
+    parser.add_argument("--text-file", type=file_check, default="./sample.txt")
 
     args = parser.parse_args()
     print("torch version", torch.__version__)
@@ -298,17 +309,29 @@ def main():
     ema_model.load_state_dict(torch.load(f"{args.save_path}/models/ema_ckpt.pt"))
     ema_model.eval()
 
-    text_words = [args.sampling_word]
-    writer_id = random.randint(0, 339)  # index for style class
-    for x_text in text_words:
-        print("Word:", x_text)
-        print("style", writer_id)
-        labels = torch.tensor([writer_id]).long().to(args.device)
+    print("Sampling paragraph")
+    # make the code to generate lines
+    lines = open(args.text_file).read()
+    fakes = []
+    gap = np.ones((64, 16))
+    max_line_width = 900
+    total_char_count = 0
+    avg_char_width = 0
+    current_line_width = 0
+    longest_word_length = max(len(word) for word in lines.strip().split(" "))
+    # print('longest_word_length', longest_word_length)
+    # s = random.randint(0, 339)#.long().to(args.device)
+    # s = random.randint(0, 161)#.long().to(args.device)
+    s = 12  # 25 #129 #201
+    for word in lines.strip().split(" "):
+        print("Word:", word)
+        print("Style:", s)
+        labels = torch.tensor([s]).long().to(args.device)
         ema_sampled_images = diffusion.sampling(
             ema_model,
             vae,
             n=len(labels),
-            x_text=x_text,
+            x_text=word,
             labels=labels,
             args=args,
             style_extractor=feature_extractor,
@@ -317,13 +340,190 @@ def main():
             character_classes=None,
             tokenizer=tokenizer,
             text_encoder=text_encoder,
+            clip_model=None,
             run_idx=None,
         )
-        save_single_images(
-            ema_sampled_images,
-            os.path.join(f"./image_samples/", f"{x_text}_style_{writer_id}.png"),
-            args,
+        # print('ema_sampled_images', ema_sampled_images.shape)
+        image = ema_sampled_images.squeeze(0)
+
+        im = torchvision.transforms.ToPILImage()(image)
+        # reshape to height 32
+        im = im.convert("L")
+        # save im
+
+        # if len(word) < 4:
+
+        im = crop_whitespace_width(im)
+
+        im = Image.fromarray(im)
+        if len(word) == longest_word_length:
+            max_word_length_width = im.width
+            print("max_word_length_width", max_word_length_width)
+        # im.save(f'./_REBUTTAL/{word}.png')
+        # Calculate aspect ratio
+        aspect_ratio = im.width / im.height
+
+        im = np.array(im)
+        # im = np.array(resized_img)
+
+        fakes.append(im)
+
+    # Calculate the scaling factor based on the longest word
+    # find the average character width of the max length word
+
+    avg_char_width = max_word_length_width / longest_word_length
+    print("avg_char_width", avg_char_width)
+    # scaling_factor = avg_char_width / (32 * aspect_ratio)  # Aspect ratio of an average character
+
+    # Scale and pad each word
+    scaled_padded_words = []
+    max_height = 64  # Defined max height for all images
+
+    for word, img in zip(lines.strip().split(" "), fakes):
+
+        img_pil = Image.fromarray(img)
+        as_ratio = img_pil.width / img_pil.height
+        # scaled_width = int(scaling_factor * len(word))#) * as_ratio * max_height)
+        scaled_width = int(avg_char_width * len(word))
+
+        scaled_img = img_pil.resize((scaled_width, int(scaled_width / as_ratio)))
+        print(f"Word {word} - scaled_img {scaled_img.size}")
+        # Padding
+        # if word is in punctuation:
+        if word in punctuation:
+            # rescale to height 10
+            w_punc = scaled_img.width
+            h_punc = scaled_img.height
+            as_ratio_punct = w_punc / h_punc
+            if word == ".":
+                scaled_img = scaled_img.resize((int(5 * as_ratio_punct), 5))
+            else:
+                scaled_img = scaled_img.resize((int(13 * as_ratio_punct), 13))
+            # pad on top and leave the image in the bottom
+            padding_bottom = 10
+            padding_top = (
+                max_height - scaled_img.height - padding_bottom
+            )  # All padding goes on top
+            # No padding at the bottom
+
+            # Apply padding
+            padded_img = np.pad(
+                scaled_img,
+                ((padding_top, padding_bottom), (0, 0)),
+                mode="constant",
+                constant_values=255,
+            )
+        else:
+            if scaled_img.height < max_height:
+                padding = (max_height - scaled_img.height) // 2
+                # print(f'Word {word} - padding: {padding}')
+                padded_img = np.pad(
+                    scaled_img,
+                    (
+                        (padding, max_height - scaled_img.height - padding),
+                        (0, 0),
+                    ),
+                    mode="constant",
+                    constant_values=255,
+                )
+            else:
+                # resize to max height while maintaining aspect ratio
+                # ar = scaled_img.width / scaled_img.height
+
+                scaled_img = scaled_img.resize(
+                    (int(max_height * as_ratio) - 4, max_height - 4)
+                )
+                padding = (max_height - scaled_img.height) // 2
+                # print(f'Word {word} - padding: {padding}')
+                padded_img = np.pad(
+                    scaled_img,
+                    (
+                        (padding, max_height - scaled_img.height - padding),
+                        (0, 0),
+                    ),
+                    mode="constant",
+                    constant_values=255,
+                )
+
+            # padded_img = np.array(scaled_img)
+        # print('padded_img', padded_img.shape)
+        scaled_padded_words.append(padded_img)
+
+    # Create a gap array (white space)
+    height = 64  # Fixed height for all images
+    gap = np.ones((height, 16), dtype=np.uint8) * 255  # White gap
+
+    # Concatenate images with gaps
+    sentence_img = gap  # Start with a gap
+    lines = []
+    line_img = gap
+    # Concatenate images with gaps
+    """
+    sentence_img = gap  # Start with a gap
+    for img in scaled_padded_words:
+        #print('img', img.shape)
+        sentence_img = np.concatenate((sentence_img, img, gap), axis=1)
+    """
+
+    for img in scaled_padded_words:
+        img_width = img.shape[1] + gap.shape[1]
+
+        if current_line_width + img_width < max_line_width:
+            # Add the image to the current line
+            if line_img.shape[0] == 0:
+                line_img = (
+                    np.ones((height, 0), dtype=np.uint8) * 255
+                )  # Start a new line
+            line_img = np.concatenate((line_img, img, gap), axis=1)
+            current_line_width += img_width  # + gap.shape[1]
+            # print('current_line_width if', current_line_width)
+            # Check if adding this image exceeds the max line width
+        else:
+            # Pad the current line with white space to max_line_width
+            remaining_width = max_line_width - current_line_width
+            line_img = np.concatenate(
+                (
+                    line_img,
+                    np.ones((height, remaining_width), dtype=np.uint8) * 255,
+                ),
+                axis=1,
+            )
+            lines.append(line_img)
+
+            # Start a new line with the current word
+            line_img = np.concatenate((gap, img, gap), axis=1)
+            current_line_width = img_width  # + 2 * gap.shape[1]
+            # print('current_line_width else', current_line_width)
+    # Add the last line to the lines list
+    if current_line_width > 0:
+        # Pad the last line to max_line_width
+        remaining_width = max_line_width - current_line_width
+        line_img = np.concatenate(
+            (
+                line_img,
+                np.ones((height, remaining_width), dtype=np.uint8) * 255,
+            ),
+            axis=1,
         )
+        lines.append(line_img)
+
+    # # Concatenate all lines to form a paragraph, pad them if necessary
+    # max_height = max([line.shape[0] for line in lines])
+    # paragraph_img = np.ones((0, max_line_width), dtype=np.uint8) * 255
+    # for line in lines:
+    #     if line.shape[0] < max_height:
+    #         padding = (max_height - line.shape[0]) // 2
+    #         line = np.pad(line, ((padding, max_height - line.shape[0] - padding), (0, 0)), mode='constant', constant_values=255)
+
+    #     #print the shapes
+    #     print('line shape', line.shape)
+    # print('paragraph shape', paragraph_img.shape)
+    paragraph_img = np.concatenate((lines), axis=0)
+
+    paragraph_image = Image.fromarray(paragraph_img)
+    paragraph_image = paragraph_image.convert("L")
+
+    paragraph_image.save(f"paragraph_style_{s}.png")
 
 
 if __name__ == "__main__":
