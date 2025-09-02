53eefefcb0eb08e476dcc91ebf6902ecfc7f3f42
Gautham <ahgamut@gmail.com>
2025-09-02 04:52:38 -0500

    remove duplicate LineListIO
    also IAMDataset is not used

--- style_encoder_train.py
+++ style_encoder_train.py
@@ -20,7 +20,7 @@ import json
 import random
 
 #
-from utils.iam_dataset import IAMDataset
+from utils.word_dataset import LineListIO
 from utils.auxilary_functions import (
     affine_transformation,
     image_resize_PIL,
@@ -568,51 +568,6 @@ class WordLineDataset(Dataset):
             print('Id = {}, Transcription = "{}"'.format(id, transcription))
             show_image(Image.fromarray(255.0 * image))
             print()
-
-
-class LineListIO(object):
-    """
-    Helper class for reading/writing text files into lists.
-    The elements of the list are the lines in the text file.
-    """
-
-    @staticmethod
-    def read_list(filepath, encoding="ascii"):
-        if not os.path.exists(filepath):
-            raise ValueError("File for reading list does NOT exist: " + filepath)
-
-        linelist = []
-        if encoding == "ascii":
-            transform = lambda line: line.encode()
-        else:
-            transform = lambda line: line
-
-        with io.open(filepath, encoding=encoding) as stream:
-            for line in stream:
-                line = transform(line.strip())
-                if line != "":
-                    linelist.append(line)
-        return linelist
-
-    @staticmethod
-    def write_list(file_path, line_list, encoding="ascii", append=False, verbose=False):
-        """
-        Writes a list into the given file object
-
-        file_path: the file path that will be written to
-        line_list: the list of strings that will be written
-        """
-        mode = "w"
-        if append:
-            mode = "a"
-
-        with io.open(file_path, mode, encoding=encoding) as f:
-            if verbose:
-                line_list = tqdm.tqdm(line_list)
-
-            for l in line_list:
-                # f.write(unicode(l) + '\n')   Python 2
-                f.write(l + "\n")
 
 
 class IAMDataset_style(WordLineDataset):
