235e3cad4b5a7a52ed052434cfbabfe76995bc38
Gautham <ahgamut@gmail.com>
2025-09-04 05:39:26 -0500

    cleanup

--- models/diffpen2.py
+++ models/diffpen2.py
@@ -27,60 +27,7 @@ from .auxiliary_functions import (
     centered_PIL,
 )
 from .feature_extractor import ImageEncoder
-
-
-class AvgMeter:
-    def __init__(self, name="Metric"):
-        self.name = name
-        self.reset()
-
-    def reset(self):
-        self.avg, self.sum, self.count = [0] * 3
-
-    def update(self, val, count=1):
-        self.count += count
-        self.sum += val * count
-        self.avg = self.sum / self.count
-
-    def __repr__(self):
-        text = f"{self.name}: {self.avg:.4f}"
-        return text
-
-
-class EMA:
-    """
-    EMA is used to stabilize the training process of diffusion models by
-    computing a moving average of the parameters, which can help to reduce
-    the noise in the gradients and improve the performance of the model.
-    """
-
-    def __init__(self, beta):
-        super().__init__()
-        self.beta = beta
-        self.step = 0
-
-    def update_model_average(self, ma_model, current_model):
-        for current_params, ma_params in zip(
-            current_model.parameters(), ma_model.parameters()
-        ):
-            old_weight, up_weight = ma_params.data, current_params.data
-            ma_params.data = self.update_average(old_weight, up_weight)
-
-    def update_average(self, old, new):
-        if old is None:
-            return new
-        return old * self.beta + (1 - self.beta) * new
-
-    def step_ema(self, ema_model, model, step_start_ema=2000):
-        if self.step < step_start_ema:
-            self.reset_parameters(ema_model, model)
-            self.step += 1
-            return
-        self.update_model_average(ema_model, model)
-        self.step += 1
-
-    def reset_parameters(self, ema_model, model):
-        ema_model.load_state_dict(model.state_dict())
+from .diffpen import AvgMeter, EMA
 
 
 def iam_resizefix(img_s):
