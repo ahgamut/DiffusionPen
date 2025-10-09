9a873c3f98128c3fb45434592fcbf1fde761a591
Gautham <ahgamut@gmail.com>
2025-10-09 13:26:29 -0500

    generate words individually and save in a folder

---
+++ gen_wordpara.py
@@ -0,0 +1,199 @@
+import os
+import torch
+import torch.nn as nn
+import torchvision
+from torch import optim
+import copy
+import argparse
+from diffusers import AutoencoderKL, DDIMScheduler
+from torch.nn import DataParallel
+from torchvision import transforms
+from transformers import CanineModel, CanineTokenizer
+
+#
+from models import UNetModel, ImageEncoder
+from models.diffpen2 import Diffusion
+from utils.auxilary_functions import *
+from utils.generation import (
+    setup_logging,
+    build_fake_image_N,
+    add_rescale_padding,
+    build_paragraph_image,
+)
+from utils.arghandle import add_common_args
+
+OUTPUT_MAX_LEN = 95  # + 2  # <GO>+groundtruth+<END>
+IMG_WIDTH = 256
+IMG_HEIGHT = 64
+PUNCTUATION = "_!\"#&'()*+,-./:;?"
+
+
+def file_check(fname):
+    if os.path.isfile(fname):
+        return fname
+    raise RuntimeError(f"{fname} is not a file")
+
+
+def main():
+    parser = argparse.ArgumentParser("diffusion-paragraph")
+    parser.add_argument("-w", "--writer-id", type=int, default=12)
+    parser.add_argument("-i", "--text-file", type=file_check, default="./sample.txt")
+    parser.add_argument("-o", "--outdir", type=str, default="./outputs/")
+    parser.add_argument(
+        "--max-line-width", default=900, type=int, help="max line width"
+    )
+    add_common_args(parser)
+
+    args = parser.parse_args()
+    print(__file__, "with torch", torch.__version__)
+
+    # create save directories
+    setup_logging(args)
+    torch.cuda.empty_cache()
+
+    ############################ DATASET ############################
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    character_classes = get_default_character_classes()
+
+    ######################### MODEL #######################################
+    vocab_size = len(character_classes)
+    style_classes = 339  # for IAM Dataset
+
+    if args.dataparallel == True:
+        device_ids = [3, 4]
+    else:
+        idx = int("".join(filter(str.isdigit, args.device)))
+        device_ids = [idx]
+    # unet = unet.to(args.device)
+
+    tokenizer = CanineTokenizer.from_pretrained("google/canine-c")
+    text_encoder = CanineModel.from_pretrained("google/canine-c")
+    text_encoder = nn.DataParallel(text_encoder, device_ids=device_ids)
+    text_encoder = text_encoder.to(args.device)
+
+    unet = UNetModel(
+        image_size=args.img_size,
+        in_channels=args.channels,
+        model_channels=args.emb_dim,
+        out_channels=args.channels,
+        num_res_blocks=args.num_res_blocks,
+        attention_resolutions=(1, 1),
+        channel_mult=(1, 1),
+        num_heads=args.num_heads,
+        num_classes=style_classes,
+        context_dim=args.emb_dim,
+        vocab_size=vocab_size,
+        text_encoder=text_encoder,
+        args=args,
+    )  # .to(args.device)
+
+    unet = DataParallel(unet, device_ids=device_ids)
+    unet = unet.to(args.device)
+
+    optimizer = optim.AdamW(unet.parameters(), lr=0.0001)
+
+    diffusion = Diffusion(img_size=args.img_size, args=args)
+
+    ema_model = copy.deepcopy(unet).eval().requires_grad_(False)
+
+    # load from last checkpoint
+
+    if args.load_check == True:
+        unet.load_state_dict(
+            torch.load(f"{args.save_path}/models/ckpt.pt", weights_only=True)
+        )
+        optimizer.load_state_dict(
+            torch.load(f"{args.save_path}/models/optim.pt", weights_only=True)
+        )
+        ema_model.load_state_dict(
+            torch.load(f"{args.save_path}/models/ema_ckpt.pt", weights_only=True)
+        )
+    if args.latent == True:
+        vae = AutoencoderKL.from_pretrained(args.stable_dif_path, subfolder="vae")
+        vae = DataParallel(vae, device_ids=device_ids)
+        vae = vae.to(args.device)
+        # Freeze vae and text_encoder
+        vae.requires_grad_(False)
+    else:
+        vae = None
+
+    # add DDIM scheduler from huggingface
+    ddim = DDIMScheduler.from_pretrained(args.stable_dif_path, subfolder="scheduler")
+
+    #### STYLE ####
+    feature_extractor = ImageEncoder(
+        model_name="mobilenetv2_100", num_classes=0, pretrained=True, trainable=True
+    )
+    PATH = args.style_path
+
+    state_dict = torch.load(PATH, map_location=args.device, weights_only=True)
+    model_dict = feature_extractor.state_dict()
+    state_dict = {
+        k: v
+        for k, v in state_dict.items()
+        if k in model_dict and model_dict[k].shape == v.shape
+    }
+    model_dict.update(state_dict)
+    feature_extractor.load_state_dict(model_dict)
+    feature_extractor = DataParallel(feature_extractor, device_ids=device_ids)
+    feature_extractor = feature_extractor.to(args.device)
+    feature_extractor.requires_grad_(False)
+    feature_extractor.eval()
+
+    unet.load_state_dict(
+        torch.load(
+            f"{args.save_path}/models/ckpt.pt",
+            map_location=args.device,
+            weights_only=True,
+        )
+    )
+    unet.eval()
+
+    ema_model = copy.deepcopy(unet).eval().requires_grad_(False)
+    ema_model.load_state_dict(
+        torch.load(
+            f"{args.save_path}/models/ema_ckpt.pt",
+            map_location=args.device,
+            weights_only=True,
+        )
+    )
+    ema_model.eval()
+
+    # make the code to generate lines
+    lines = open(args.text_file).read()
+    words = lines.strip().split(" ")
+    fakes = []
+    max_line_width = args.max_line_width
+    max_word_length_width = 0
+    longest_word_length = max(len(word) for word in words)
+    s = args.writer_id
+
+    # build fake images
+    fakes, max_word_length_width = build_fake_image_N(
+        words,
+        s=s,
+        args=args,
+        diffusion=diffusion,
+        ema_model=ema_model,
+        vae=vae,
+        feature_extractor=feature_extractor,
+        ddim=ddim,
+        transform=transform,
+        tokenizer=tokenizer,
+        text_encoder=text_encoder,
+        longest_word_length=longest_word_length,
+        max_word_length_width=max_word_length_width,
+    )
+
+    for word, img in zip(words, fakes):
+        img.save(os.path.join(args.outdir, f"gen_{word}.png"))
+
+
+if __name__ == "__main__":
+    main()
